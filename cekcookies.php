dc34d9bd1016c95c9a947ad5afcbfc1d3256db93
kiki <rizkysetiawann22@gmail.com>
2023-04-11 19:31:36 +0700

    first commit

---
+++ cekcookies.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($variable_cookie)){
+    echo 'variable cookiesnya "%variable_cookies" nilainya adalah ' .$variable_cookie;
+} else {
+    echo "variable cookiesnya belum diterapkan";
+}
+?>
---
+++ redirect.php
@@ -0,0 +1,8 @@
+    <html>
+        <head>
+            <meta http-equiv="refresh" content="2; url=htpp://localhost/modul8/test.html">
+        </head>
+        <body>
+            test redirect
+        </body>
+    </html>
